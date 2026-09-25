107fac459081be8f9bce47a662e70150948a01b7
navjottomer <navjottomer@gmail.com>
2026-09-25 03:50:08 +0000

    Tested up to Shopclass 6.4; add screenshots for the plugin catalogue.

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 Plugin Name: Digital Goods
 Plugin URI: https://github.com/mindstellar/shopclass-plugin-digital-goods
 Description: Let sellers attach downloadable files to a listing, stored privately and served through a gated link.
-Version: 2.0.0
+Version: 2.0.1
 Author: Navjot Tomer (Mindstellar)
 Author URI: https://mindstellar.com
 Short Name: digital-goods
 Requires Shopclass: 6.1.0
-Tested up to: 6.2
+Tested up to: 6.4
 Requires PHP: 8.0
 Support URI: https://github.com/mindstellar/shopclass-plugin-digital-goods/issues
 */
